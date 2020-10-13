1daeef57fd0c19a19a033de488b507eab202f7f2
hugoblm <38689697+hugoblm@users.noreply.github.com>
2020-10-13 10:30:53 +0200

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'JYMIZ', page: '/home' },
+  { label: 'Home', page: '/home' },
   { label: 'Lifestyle', page: '/blog' },
   { label: 'Sport', page: '/blog' },
   { label: 'Nutrition', page: '/blog' },
